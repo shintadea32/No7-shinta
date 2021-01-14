9730e23f38d79d9a108698553a2592c01e4df289
shintadewi29rpl <shinta_dewi_29rpl@gmail.com>
2021-01-14 23:52:16 +0700

    Project Ketiga

---
+++ src/tujuh/Tujuh.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tujuh;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Tujuh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }//spasi 
+        int zx = 4;
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+    }
+    }
+    
+
+
+
